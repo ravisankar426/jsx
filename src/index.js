b2fed2f937614f7f1a36b8b37c147b0c90c85d33
ravisankar-uppu <ravisankar_uppu@yahoo.co.in>
2024-11-17 00:25:57 -0600

    commit for the jsx related learning changes.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import the react libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// find the div with id root
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// take control of the div using react
+const root = ReactDOM.createRoot(el);
+
+// show the component on the screen
+root.render(<App />);
